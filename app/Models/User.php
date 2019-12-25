ad934e73838096bccd4a98e1966cb4ec6f10a8d6
SilenceXiao <1282288696@qq.com>
2019-12-25 08:50:47 +0000

    修改User.php命名空间，移动到指定位置

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,9 +10,10 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = 'users';
     /**
      * The attributes that are mass assignable.
-     *
+     * 过滤用户提交的字段，存在时才会更新
      * @var array
      */
     protected $fillable = [
@@ -21,7 +22,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     *
+     *  隐藏属性
      * @var array
      */
     protected $hidden = [
