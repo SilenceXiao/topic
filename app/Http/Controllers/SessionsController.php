cf402604833aca1f75366cb5f7f9c30dd65b368d
SilenceXiao <1282288696@qq.com>
2020-01-07 07:48:30 +0000

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -36,10 +36,18 @@ class SessionsController extends Controller
     	]);
 
     	if(Auth::attempt($user, $request->has('remember'))){ //匹配用户成功
-    		session()->flash('success','登陆成功');
-    		$fallback = route('users.show', Auth::user());
-    		// return redirect()->route('users.show',[Auth::user()]);
-           	return redirect()->intended($fallback);
+            if(Auth::user()->activated){
+
+        		session()->flash('success','登陆成功');
+        		$fallback = route('users.show', Auth::user());
+        		// return redirect()->route('users.show',[Auth::user()]);
+               	return redirect()->intended($fallback);
+            }else{
+
+                Auth::logout();
+                session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+                return redirect('/');
+            }
 
     	}else{//匹配用户失败
 
